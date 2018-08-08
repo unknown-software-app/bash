968f414f8392d78b947e5ba15e32d9780e73fd52
unknown-software-app <39619620+unknown-software-app@users.noreply.github.com>
2018-08-08 18:41:01 +0300

    Update dockerDeployment.bash

--- dockerDeployment.bash
+++ dockerDeployment.bash
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Docker deployment script
 
+# container parameters
+IMAGE_NAME=$1
+DOCKER_TAG=$2
+PORT=$3
+ENV_VAR=$4
+VOL_NAME=$5
+NETWROK=$6
 
 # check if docker cli is exist. if not, install it
 type docker 2>&1
@@ -11,32 +18,35 @@ if [ $? -eq 1 ] ; then
 fi
 
 
-
-
-#download latest image
-sudo docker pull hello-world:latest
+#download DOCKER_TAG image
+sudo docker pull $IMAGE_NAME:$DOCKER_TAG
 
 # stop and remove exist container, if exist
-sudo docker ps -a | grep hello-world
+sudo docker ps -a | grep $IMAGE_NAME
 if [ $? -eq 0 ] ; then
-	sudo docker stop hello-world
-	sudo docker rm hello-world
+	sudo docker stop $IMAGE_NAME
+	sudo docker rm $IMAGE_NAME
 fi
 
 # check if docker image with rollback tag is exist, if yes, remove it
-sudo docker images | grep hello-world  | grep rollback
+sudo docker images | grep $IMAGE_NAME  | grep rollback
 if [ $? -eq 0 ] ; then
-	sudo docker rmi hello-world:rollback
+	sudo docker rmi $IMAGE_NAME:rollback
 fi
 
 # check if docker image with current tag is exist, if yes, tag it as rollback
-sudo docker images | grep hello-world  | grep current
+sudo docker images | grep $IMAGE_NAME  | grep current
 if [ $? -eq 0 ] ; then
-	sudo docker tag hello-world:current hello-world:rollback
+	sudo docker tag $IMAGE_NAME:current $IMAGE_NAME:rollback
 fi
 
 # tag latest image as current
-sudo docker tag hello-world:latest hello-world:current
+sudo docker tag $IMAGE_NAME:latest $IMAGE_NAME:current
 
 # run docker container with new image.
-sudo docker run -d --name hello-world hello-world:current 
+sudo docker run -d \
+-p $PORT:$PORT \
+-e ENV=$ENV_VAR \
+-v $VOL_NAME \
+--network $NETWROK \
+--name $IMAGE_NAME $IMAGE_NAME:current 
